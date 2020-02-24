f5a29c3aee75b80da916e5aa49c240ff04eabd0f
Mallikarjuna Bandi <Mallikarjuna_Bandi@epam.com>
2020-02-24 12:00:55 +0100

    Component deep dive practice

--- lists-conditionals/src/Components/Persons/Persons.js
+++ lists-conditionals/src/Components/Persons/Persons.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Person from './Person/Person'
 
-const persons = (props) => props.persons.map((person, index)=>{
-    return <Person
-     key={person.id}
-     click ={()=> props.click(index)}
-     name={person.name}
-     age={person.age}
-     changed={(event)=>props.changed(event,person.id)}
-     />
-});
+class Persons extends Component{
+   render(){
+         return this.props.persons.map((person, index)=>{
+            return (<Person
+            key={person.id}
+            click ={()=> this.props.click(index)}
+            name={person.name}
+            age={person.age}
+            changed={(event)=>this.props.changed(event,person.id)}
+            />)
+            
+          });
+    }
 
-export default persons;
+} 
+
+export default Persons;
