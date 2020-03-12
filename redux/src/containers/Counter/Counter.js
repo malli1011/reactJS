0d5809e3bf3d9ccd0a4889c4b5a3d9792a4d99a4
Mallikarjuna Bandi <Mallikarjuna_Bandi@epam.com>
2020-03-12 16:06:07 +0100

    Uses redux tools and redux-thunk for async calls

--- redux/src/containers/Counter/Counter.js
+++ redux/src/containers/Counter/Counter.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import {increment,decrement,add,substract,storeResult,deleteResult} from '../../store/actions';
 
 class Counter extends Component {
-    
-    render() {
+    render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
-                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
-    return { ctr: state.counter }
-}
+const mapStateToProps = state => {
+    return {
 
-const mapDispatchToProps = (dispatch) => {
-    return{
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-        onDecrementCounter : () => dispatch({ type: 'DECREMENT' }),
-        onAddCounter : ()=> dispatch({type:'ADD',value:5}),
-        onSubstractCounter :()=> dispatch({type:'SUBSTRACT',value:5})
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     }
-    
-    
-}
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(Counter);
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch(increment()),
+        onDecrementCounter: () => dispatch(decrement()),
+        onAddCounter: () => dispatch(add(10)),
+        onSubtractCounter: () => dispatch(substract(15)),
+        onStoreResult: (result) => dispatch(storeResult(result)),
+        onDeleteResult: (id) => dispatch(deleteResult(id))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ redux/src/store/actions.js
@@ -0,0 +1,59 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD = 'ADD';
+export const SUBTRACT = 'SUBTRACT';
+export const STORE_RESULT = 'STORE_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
+
+export const increment = () => {
+    return{
+        type: INCREMENT
+    }
+};
+
+export const decrement = () => {
+    return{
+        type: DECREMENT
+    }
+};
+
+export const add = (value) => {
+    return{
+        type: ADD,
+        val:value
+    }
+};
+
+export const substract = (value) => {
+    return{
+        type: SUBTRACT,
+        val:value
+    }
+};
+
+export const saveResult = (value) => {
+    return{
+        type: STORE_RESULT,
+        result:value
+    }
+};
+
+
+
+export const storeResult = (value) => {
+    return dispatch =>{
+        setTimeout(()=>{
+            dispatch(saveResult(value))
+        },5000);
+        
+    }
+};
+
+
+export const deleteResult = (value) => {
+    return{
+        type: DELETE_RESULT,
+        resultElId:value
+    }
+};
+
